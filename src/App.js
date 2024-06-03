465d20ab925d88fac71ab3cc014c6cbe3dfa5546
Felipegithubf <felipecarvalho5055@gmail.com>
2024-06-03 01:28:28 -0300

    alteração

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import './App.css';
-
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
 export default () => {
   return (
-    <div className='app-widow'>
-     <div className='sidebar'>
+    <div className='app-window'>
+      <div className='sidebar'>
+        <header>
+          <img className="header--avatar" src="ilustracao-3d-de-avatar-ou-perfil-humano_23-2150671142.jpg" alt="" />
+          <div className='header--button'>
+            
+           <div className="header--btn">
+                      <DonutLargeIcon style={{color: '#919191'}} />
+           </div>
+           <div className="header--btn">
+                      <ChatIcon style={{color: '#919191'}} />
+           </div>
+           <div className="header--btn">
+                      <MoreVertIcon style={{color: '#919191'}} />
+           </div>
 
-     </div>
-      <div className="conntentarea">
+
+          </div>
+        </header>
+
+        <div className='search'>
+          <div className="search--input">
+           <SearchIcon fontSize="small" style={{color: '#919191'}} />
+           <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+          </div>
+        </div>
+
+        <div className='chatlist'>
+      
+        </div>
+      </div>
+
+      <div className="content-area">
         
       </div>
     </div>
-  )
-}
+  );
+};
