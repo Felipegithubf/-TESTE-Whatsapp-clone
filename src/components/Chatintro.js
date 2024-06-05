e0af7b59c3a8c614e54cdc365868f07cc1a845e0
Felipegithubf <felipecarvalho5055@gmail.com>
2024-06-05 17:33:04 -0300

    alteração

---
+++ src/components/Chatintro.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Chatintro.css';
+
+
+
+export default () => {
+    return  (
+        <div className="chatIntro">
+            <img src="intro-whatsapp-620433d4.jpg" alt="" />
+            <h1>Mantenha seu celular Conectado</h1>
+            
+            <h2>O Whatsapp conecta ao seu telefone pra sincronizar suas mensagens <br/>
+            
+            Para Reduzir o uso de dados, conecte seu telefone na rede wifi</h2>
+        </div>
+    )
+}
